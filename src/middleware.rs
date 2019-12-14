d1cc192a21fa19ac1f147df37f738d7f877685ec
Max Countryman <maxc@me.com>
2019-12-14 07:14:39 -0800

    provide middleware default error type

--- src/middleware.rs
+++ src/middleware.rs
@@ -1,7 +1,8 @@
 use crate::handler::Handler;
+use crate::error;
 
 /// Middleware type alias.
-pub type Middleware<E> = Box<dyn Fn(Handler<E>) -> Handler<E>>;
+pub type Middleware<E = error::Error> = Box<dyn Fn(Handler<E>) -> Handler<E>>;
 
 /// A trait which enables wrapping of middleware around the handler.
 pub trait HasMiddleware<E> {
